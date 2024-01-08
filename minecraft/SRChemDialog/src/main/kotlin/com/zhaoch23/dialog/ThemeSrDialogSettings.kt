058420710fc67da1334274d520687fd893e492e6
zhaoch23 <c233zhao@uwaterloo.ca>
2024-01-08 16:54:17 -0500

    Initial Commit

---
+++ minecraft/SRChemDialog/src/main/kotlin/com/zhaoch23/dialog/ThemeSrDialogSettings.kt
@@ -0,0 +1,6 @@
+package com.zhaoch23.dialog
+
+import ink.ptms.chemdah.core.conversation.theme.ThemeSettings
+import ink.ptms.chemdah.taboolib.library.configuration.ConfigurationSection
+
+class ThemeSrDialogSettings(root: ConfigurationSection) : ThemeSettings(root)
