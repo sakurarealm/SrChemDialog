b67385855f034b88ef1a4de175bf00f8acbf3657
Zhaoch <c233zhao@uwaterloo.ca>
2024-01-08 17:01:41 -0500

    Delete minecraft/SRChemDialog directory

--- minecraft/SRChemDialog/src/main/kotlin/com/zhaoch23/dialog/ThemeSrDialogSettings.kt
+++
@@ -1,6 +0,0 @@
-package com.zhaoch23.dialog
-
-import ink.ptms.chemdah.core.conversation.theme.ThemeSettings
-import ink.ptms.chemdah.taboolib.library.configuration.ConfigurationSection
-
-class ThemeSrDialogSettings(root: ConfigurationSection) : ThemeSettings(root)
